b290f58f0592b03447babacec4067a0a33a44ddd
Oliver Just-De Bleser <47676935+just-oliver@users.noreply.github.com>
2024-06-30 18:12:34 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,29 @@
 import streamlit as st
 import pandas as pd
+import psycopg2 as psql
 
 
-st.secrets
+conn = psql.connect(database = 'pagila',
+                    user = st.secrets["sql_user"],
+                    host = st.secrets["host"],
+                    password = st.secrets["sql_password"],
+                    port=5432
+                    )
+
+cur = conn.cursor()
+
+query_sql = '''
+            SELECT
+                *
+            FROM
+                student.ojdb_matches
+            OFFSET
+            LIMIT 1;
+'''
+
+cur.execute(query_sql)
+
+row = cur.fetchone()
+
+
+st.markdown(f"## :red[Match ID : {row[0]}]")
