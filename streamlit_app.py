4c18b0d36d9a27f4d61728890544037403bfc420
Oliver Just-De Bleser <47676935+just-oliver@users.noreply.github.com>
2024-07-01 11:54:47 +0000

    hero's items

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ conn = psql.connect(database = 'pagila',
                     )
 
 cur = conn.cursor()
-
+st.title('Dota 2 Match Gussing Game')
 rank = st.selectbox('Rank of Game', ['Herald',
                                      'Guardian',
                                      'Crusader',
@@ -44,26 +44,29 @@ cur.execute(query_sql)
 
 row = cur.fetchone()
 match_id = row[0]
+## Page Start
+
 st.markdown(f"## :red[Match ID : {match_id}]")
 ## Get heroes
 hero_sql = f'''
             SELECT *
             FROM student.ojdb_hero_picks
             WHERE match_id = {match_id}
-            ORDER BY team ASC;
+            ORDER BY team ASC; -- gets the radiant heroes first followed by dire
 '''
 cur.execute(hero_sql)
 hero_rows = cur.fetchall()
 hero_ids = [str(hero_row[1]) for hero_row in hero_rows]
+facets = [hero_row[3] for hero_row in hero_rows]
 
+# Getting hero names and image locations from json
 with open('data/heroes.json', 'r') as f:
     hero_id_dict = json.load(f)
-
 
 items_ids = [hero_row[4] for hero_row in hero_rows]
 # int to str
 items_ids = [[str(x) for x in item_ids] for item_ids in items_ids]
-
+hero_details = [(hero_id_dict[hero_id]['localized_name'], hero_id_dict[hero_id]['img']) for hero_id in hero_ids]
 
 with open('data/item_ids.json', 'r') as f:
     item_id_dict = json.load(f)
@@ -72,19 +75,17 @@ items = [[item_id_dict[x] for x in item_ids] for item_ids in items_ids]
 with open('data/items.json', 'r') as f:
     item_dict = json.load(f)
 
-
-
 items_details = [[(item_dict[item]['dname'],item_dict[item]['img']) for item in item_list if item ] for item_list in items]
 
-hero_details = [(hero_id_dict[hero_id]['localized_name'], hero_id_dict[hero_id]['img']) for hero_id in hero_ids]
+
 
 
 cols = st.columns(2)
 with cols[0]:
     st.header(':green[Radiant Team]')
     for i, hero in enumerate(hero_details[:5]):
-        st.image('https://cdn.cloudflare.steamstatic.com/' + hero[1], caption=hero[0])
-        st.markdown('Items')
+        st.image('https://cdn.cloudflare.steamstatic.com/' + hero[1], caption=hero[0], width=200)
+        st.markdown(f"{hero[0]}'s Items")
         item_cols = st.columns(3)
         items = items_details[i]
         for j,item in enumerate(items):
@@ -93,7 +94,7 @@ with cols[0]:
                     st.image('./images/empty.png', caption=items[j][0],width=88)
                 else:
                     if len(items[j][0]) > 12:
-                        st.image('https://cdn.cloudflare.steamstatic.com/'+items[j][1], caption=items[j][0][:13])
+                        st.image('https://cdn.cloudflare.steamstatic.com/'+items[j][1], caption=items[j][0][:13] + '...')
                     else:
                         st.image('https://cdn.cloudflare.steamstatic.com/'+items[j][1], caption=items[j][0])
 
@@ -102,8 +103,8 @@ with cols[1]:
     st.header(':red[Dire Team]')
     for i, hero in enumerate(hero_details[5:]):
         i = i + 5
-        st.image('https://cdn.cloudflare.steamstatic.com/' + hero[1], caption=hero[0],)
-        st.markdown('Items')
+        st.image('https://cdn.cloudflare.steamstatic.com/' + hero[1], caption=hero[0], width=200)
+        st.markdown(f"{hero[0]}'s Items")
         item_cols = st.columns(3)
         items = items_details[i]
         for j,item in enumerate(items):
@@ -112,7 +113,7 @@ with cols[1]:
                     st.image('./images/empty.png', caption=items[j][0],width=88)
                 else:
                     if len(items[j][0]) > 12:
-                        st.image('https://cdn.cloudflare.steamstatic.com/'+items[j][1], caption=items[j][0][:13])
+                        st.image('https://cdn.cloudflare.steamstatic.com/'+items[j][1], caption=items[j][0][:13] + '...')
                     else:
                         st.image('https://cdn.cloudflare.steamstatic.com/'+items[j][1], caption=items[j][0])
 ## Choice Form
