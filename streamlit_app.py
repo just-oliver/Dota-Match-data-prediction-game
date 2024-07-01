7245dd747ca8f59317f085d8713f3eaee23d2e37
Oliver Just-De Bleser <47676935+just-oliver@users.noreply.github.com>
2024-07-01 14:22:31 +0000

    Funcitioning game with bug removed

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,15 +3,16 @@ import pandas as pd
 import psycopg2 as psql
 import json
 
-conn = psql.connect(database = 'pagila',
-                    user = st.secrets["sql_user"],
-                    host = st.secrets["host"],
-                    password = st.secrets["sql_password"],
-                    port=5432
-                    )
 
-cur = conn.cursor()
 st.title('Dota 2 Match Gussing Game')
+
+if 'score' not in st.session_state:
+    st.session_state.score = 0
+
+
+st.header(f"Score: {st.session_state.score}")
+
+
 rank = st.selectbox('Rank of Game', ['Herald',
                                      'Guardian',
                                      'Crusader',
@@ -29,30 +30,59 @@ rank_dists = {'Herald':(10,15),
             'Ancient':(60,65),
             'Divine':(70,75),
             'Immortal':(80,85)}
-
 dist = rank_dists[rank]
+conn = psql.connect(database = 'pagila',
+                    user = st.secrets["sql_user"],
+                    host = st.secrets["host"],
+                    password = st.secrets["sql_password"],
+                    port=5432
+                    )
 
-query_sql = f'''
+cur = conn.cursor()
+
+
+
+@st.cache_data
+def match_selection(dist):
+    '''
+    Creating a function here to cache the matchid and outcome 
+    to prevent the form submission from over writing the previous match
+    '''
+    conn = psql.connect(database = 'pagila',
+                    user = st.secrets["sql_user"],
+                    host = st.secrets["host"],
+                    password = st.secrets["sql_password"],
+                    port=5432
+                    )
+
+    cur = conn.cursor()
+    
+
+    
+
+    query_sql = f'''
             SELECT *
             FROM student.ojdb_matches
             WHERE avg_rank BETWEEN {dist[0]} AND {dist[1]}
             ORDER BY RANDOM()
             LIMIT 1
-'''
+    '''
+    cur.execute(query_sql)
+    row = cur.fetchone()
+    match_id = row[0]
+    radiant_wins = row[2]
+    return match_id, radiant_wins
 
-cur.execute(query_sql)
-
-row = cur.fetchone()
-match_id = row[0]
+match_id, radiant_wins = match_selection(dist)
 ## Page Start
 
-st.markdown(f"## :red[Match ID : {match_id}]")
+
 ## Get heroes
 hero_sql = f'''
             SELECT *
             FROM student.ojdb_hero_picks
             WHERE match_id = {match_id}
-            ORDER BY team ASC; -- gets the radiant heroes first followed by dire
+            ORDER BY team ASC, net_worth DESC; -- gets the radiant heroes first followed by dire, then top networth heroes
 '''
 cur.execute(hero_sql)
 hero_rows = cur.fetchall()
@@ -117,13 +147,27 @@ with cols[1]:
                     else:
                         st.image('https://cdn.cloudflare.steamstatic.com/'+items[j][1], caption=items[j][0])
 ## Choice Form
+
+
 with st.form('Player Guess'):
     st.markdown('**Predict which team will win!**')
     selection = st.radio('Team:', ['Radiant','Dire'])
+
     submition_button = st.form_submit_button()
 
+
 if submition_button:
-    if selection == 'Radiant':
-        st.markdown(f'You have guessed the **:green[Radiant]** team will win!')
+    st.write(selection)
+    if (radiant_wins and selection == 'Radiant') or not (radiant_wins and selection == 'Dire'):
+        st.markdown(f'You have Guessed correctly and gained **100 points**! 🙌')
+        st.session_state.score += 100
+        st.balloons()
+        
     else:
-        st.markdown(f'You have guessed the **:red[Dire]** team will win!')
+        st.markdown(f'You have Guessed incorrectly and lose **100 points**! 👎')
+        st.session_state.score -= 100
+    st.markdown(f"## [:red[Match ID : {match_id}]](https://www.dotabuff.com/matches/{match_id})")
+
+if st.button("New Game"):
+        st.cache_data.clear()
+        st.rerun()
