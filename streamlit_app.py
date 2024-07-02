3eeecbaf8c3ea9294553bd05ab380a04a6872018
Oliver Just-De Bleser <47676935+just-oliver@users.noreply.github.com>
2024-07-02 11:13:50 +0000

    added json started facets and backpacks

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,11 @@ rank = st.selectbox('Rank of Game', ['Herald',
 
 st.divider()
 st.markdown("<h2 style='text-align: center;'>Hero Picks</h2>", unsafe_allow_html=True)
+# check box to show items
+see_items = st.checkbox(f'Show Items')
+see_backpack = st.checkbox(f'Show Backback')
 
+#
 rank_dists = {'Herald':(10,15),
             'Guardian':(20,25),
             'Crusader':(30,35),
@@ -93,24 +97,34 @@ cur.execute(hero_sql)
 hero_rows = cur.fetchall()
 hero_ids = [str(hero_row[1]) for hero_row in hero_rows]
 facets = [hero_row[3] for hero_row in hero_rows]
+st.write(facets)
 
 # Getting hero names and image locations from json
 with open('data/heroes.json', 'r') as f:
     hero_id_dict = json.load(f)
 
+hero_details = [(hero_id_dict[hero_id]['localized_name'], hero_id_dict[hero_id]['img']) for hero_id in hero_ids]
+
 items_ids = [hero_row[4] for hero_row in hero_rows]
+backpacks_ids = [hero_row[5] for hero_row in hero_rows]
 # int to str
 items_ids = [[str(x) for x in item_ids] for item_ids in items_ids]
-hero_details = [(hero_id_dict[hero_id]['localized_name'], hero_id_dict[hero_id]['img']) for hero_id in hero_ids]
+backpacks_ids = [[str(x) for x in backpack_ids] for backpack_ids in backpacks_ids]
+
+
 
 with open('data/item_ids.json', 'r') as f:
     item_id_dict = json.load(f)
+
 items = [[item_id_dict[x] for x in item_ids] for item_ids in items_ids]
+backpacks = [[item_id_dict[x] for x in backpack_ids] for backpack_ids in backpacks_ids]
+
 
 with open('data/items.json', 'r') as f:
     item_dict = json.load(f)
 
-items_details = [[(item_dict[item]['dname'],item_dict[item]['img']) for item in item_list if item ] for item_list in items]
+items_details = [[(item_dict[item]['dname'],item_dict[item]['img']) for item in item_list] for item_list in items]
+backpacks_details = [[(item_dict[item]['dname'],item_dict[item]['img']) for item in backpack_list] for backpack_list in backpacks]
 
 
 
@@ -120,7 +134,6 @@ with cols[0]:
     st.header(':green[Radiant Team]')
     for i, hero in enumerate(hero_details[:5]):
         st.image('https://cdn.cloudflare.steamstatic.com/' + hero[1], caption=hero[0], width=200)
-        see_items = st.checkbox(f'Show {hero[0]} Items')
         if see_items:
             st.markdown(f"{hero[0]}'s Items")
             item_cols = st.columns(3)
@@ -134,6 +147,9 @@ with cols[0]:
                             st.image('https://cdn.cloudflare.steamstatic.com/'+items[j][1], caption=items[j][0][:11] + '...')
                         else:
                             st.image('https://cdn.cloudflare.steamstatic.com/'+items[j][1], caption=items[j][0])
+        if see_backpack:
+            st.markdown(f"{hero[0]}'s Backpack")
+
 
 
 with cols[1]:
@@ -141,7 +157,6 @@ with cols[1]:
     for i, hero in enumerate(hero_details[5:]):
         i = i + 5
         st.image('https://cdn.cloudflare.steamstatic.com/' + hero[1], caption=hero[0], width=200)
-        see_items = st.checkbox(f'Show {hero[0]} Items')
         if see_items:
             st.markdown(f"{hero[0]}'s Items")
             item_cols = st.columns(3)
